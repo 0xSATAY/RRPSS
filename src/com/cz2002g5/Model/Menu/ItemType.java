c063e1d8c83f101111983701be4aadd5d043b386
wangdian <wadisthis26@gmail.com>
2021-11-06 21:34:15 +0800

    Add promo to itemtype

--- src/com/cz2002g5/Model/Menu/ItemType.java
+++ src/com/cz2002g5/Model/Menu/ItemType.java
@@ -1,21 +1,16 @@
 package com.cz2002g5.Model.Menu;
 
-/**
- * Enum for the type of every menu item.
- */
+/** Enum for the type of every menu item. */
 public enum ItemType {
-  /**
-   *Mains item type.
-   */
-MAINS,
+  /** Mains item type. */
+  MAINS,
 
-  /**
-   *Drinks item type.
-   */
-DRINKS,
+  /** Drinks item type. */
+  DRINKS,
 
-  /**
-   *Dessert item type.
-   */
-DESSERT
+  /** Dessert item type. */
+  DESSERT,
+
+  /** Promo item type.*/
+  PROMO
 }
